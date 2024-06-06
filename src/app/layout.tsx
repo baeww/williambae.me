fc1e2d6c522868ac1881f17f85f22ca879160f17
William Bae <wbae@vt.edu>
2024-06-06 14:04:39 -0400

    playing with defer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" async></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js" defer></script>
       </head>
       <body>
       <div className='position: absolute w-full h-full -z-10'> 
